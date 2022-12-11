8c2333ef761d264c0e8516b4ab65c6a91a8b6b8c
busstop1982 <jesus@fstph.at>
2022-12-12 00:04:47 +0100

    day3 part2 done

--- 2022/03/run.py
+++ 2022/03/run.py
@@ -1,3 +1,5 @@
+from itertools import islice
+
 minis = dict([(chr(i),i-96) for i in range(97,123)])
 maxis = dict([(chr(i),i-38) for i in range(65,91)])
 all = minis | maxis
@@ -5,11 +7,18 @@ all = minis | maxis
 sum = 0
 
 with open("input",'r') as file:
-    for line in file:
-        splitline = list(line)
-        splitline.pop(-1)
-        set1 = set(splitline[len(splitline)//2:])
-        set2 = set(splitline[:len(splitline)//2])
-        intersection = set1 & set2
-        sum += all[intersection.pop()]
+    while True:
+        lines_gen = list(islice(file, 3))
+        if not lines_gen:
+            break
+        foo = set()
+        for line in lines_gen:
+            bar = set(line)
+            bar.discard('\n')
+            if foo.isdisjoint(bar):
+                foo = foo | bar
+            else:
+                foo = foo & bar
+        print(foo)
+        sum += all[foo.pop()]
 print(sum)
