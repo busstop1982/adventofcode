235e8ae2b8b0087dffb0f7cd04a5c277a5f3c1d9
Ulrich Lazar <jesus@fstph.at>
2022-12-12 14:12:05 +0100

    day4 part2

--- 2022/04/run.py
+++ 2022/04/run.py
@@ -5,6 +5,6 @@ with open("input",'r') as file:
         numbers = [int(item) for sublist in ranges for item in sublist]
         set1 = set(range(numbers[0],numbers[1]+1))
         set2 = set(range(numbers[2],numbers[3]+1))
-        if set1.issubset(set2) or set2.issubset(set1):
+        if not set1.isdisjoint(set2):
             out += 1
 print(out)
