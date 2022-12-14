96c1a1b1be2000d78a6e2a88f1b9745db591feca
Ulrich Lazar <jesus@fstph.at>
2022-12-14 14:37:44 +0100

    day6 part2

--- 2022/06/run.py
+++ 2022/06/run.py
@@ -1,9 +1,8 @@
 with open("input", 'r') as file:
     for line in file:
         for i in range(0,len(line)):
-            block = set(line[i:i+4])
-            #print(line[i:i+3],block)
-            if len(block) == 4:
-                print(i+4)
+            block = set(line[i:i+14])
+            if len(block) == 14:
+                print(i+14)
                 break
             
